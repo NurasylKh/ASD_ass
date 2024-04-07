e69df35f05ebfcfffa43c5cb9e67e2a8ae5b48a6
ASUS <231001@astanait.edu.kz>
2024-04-07 22:50:04 +0500

    1

--- src/Task3.java
+++ src/Task3.java
@@ -1,2 +1,27 @@
-public class Task3 {
+//program which checks if number composite or prime
+//program reads number from concole and gives only answer without number
+import java.util.Scanner;
+
+public class Task3 {//method that runs program
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();//read number check from consolle
+        boolean isPrime = isPrime(n);//checks if  number prime
+        if (isPrime) {//loop that checks for prime and composite
+            System.out.println("prime");
+        } else {
+            System.out.println("composite");
+        }
+    }
+    public static boolean isPrime(int n) {//Checks if a given number is prime
+        if (n < 2) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {//check if number is divisible by any number up to its square root
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
