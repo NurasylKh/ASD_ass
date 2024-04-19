bd36b59d5e72a67a098157e5899b77259d40a3f3
ASUS <231001@astanait.edu.kz>
2024-04-19 11:44:17 +0500

    Defence_ass1

---
+++ src/Defence.java
@@ -0,0 +1,40 @@
+//given sequence of numbers you have to find accurance of specific number
+/*
+public class Defence {
+    public  static void main(String[] args){
+        int [] array = {1,2,3,4,5,6,7,8,9,10};
+        int number = 4;
+        int count=0;
+
+        for(int num:array){
+            if(num==number){
+                count++;
+            }
+        }
+
+    System.out.println(count);
+    }
+}
+*/
+
+
+public class Defence{
+    public static void main(String[] args){
+        int [] array = {1,2,3,4,5,6,7,8,9,10};
+        int number = 4;
+        int a;
+
+        System.out.println(findAccurance);
+    }
+
+
+
+    public static class findAccurance(int[] array,int a){
+        if(a==0){
+            return 0;
+        }
+
+    }
+
+}
+
